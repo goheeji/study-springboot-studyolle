c052a5693834b7c0d3c1d01f40a9b641d7fb6209
goheeji <gmlwl0489@daum.net>
2021-03-05 13:47:44 +0900

    Initial commit

---
+++ src/main/java/com/studyolle/domain/Account.java
@@ -0,0 +1,57 @@
+package com.studyolle.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    //-- 회원가입 --//
+    @Column(unique = true)
+    private String mail;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt; //가입날짜
+
+    //-- 프로필 --//
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;    // varchar(255)
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    //-- 알림설정 --//
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollResultByEmail;
+
+    private boolean studyEnrollResultByWeb;
+
+    private boolean studyUpdatedResultByEmail;
+
+    private boolean studyUpdatedResultByWeb;
+
+
+}
