1c7830a4ecc96f22e3bd228eada8c91b4d769264
goheeji <gmlwl0489@daum.net>
2021-03-05 23:25:32 +0900

    8. 회원 가입 리팩토링 및 테스트 - 리팩토링 완료

--- src/main/java/com/studyolle/account/AccountController.java
+++ src/main/java/com/studyolle/account/AccountController.java
@@ -21,8 +21,8 @@ import javax.validation.Valid;
 public class AccountController {
 
     private final SignUpFormValidator signUpFormValidator;
-    private final AccountRepository accountRepository;
-    private final JavaMailSender javaMailSender;
+    private final AccountService accountService;
+
 
     @InitBinder("signUpForm") //signUpForm을 받을때 303검증(if)과 벨리데이터까지
     public void initBinder(WebDataBinder webDataBinder){
@@ -43,27 +43,11 @@ public class AccountController {
         return "account/sign-up";
     }
 
-    Account account = Account.builder()
-            .email(signUpForm.getEmail())
-            .nickname(signUpForm.getNickname())
-            .password(signUpForm.getPassword()) //TODO encoding 해야함
-            .studyCreatedByWeb(true)
-            .studyEnrollResultByWeb(true)
-            .studyUpdatedResultByWeb(true)
-            .build();
-
-    Account newAccount = accountRepository.save(account);
-
-    newAccount.generateEmailCheckToken();
-    SimpleMailMessage mailMessage = new SimpleMailMessage();
-    mailMessage.setTo(newAccount.getEmail());
-    mailMessage.setSubject("스터디올래, 회원가입 인증"); //메일 제목
-    mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
-            "&email="+newAccount.getEmail());
-    javaMailSender.send(mailMessage);
+    accountService.processNewAccount(signUpForm);
 
     return "redirect:/";
 
 }
+
 }
 
---
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -0,0 +1,49 @@
+package com.studyolle.account;
+
+import com.studyolle.domain.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+    public void processNewAccount(SignUpForm signUpForm) {
+        //넘너온 폼을 가지고 계정을 생성하고
+        Account newAccount = saveNewAccount(signUpForm);
+        //email 체크 토큰을 만들고
+        newAccount.generateEmailCheckToken();
+        //가입확인 email을 보낸다.
+        sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .email(signUpForm.getEmail())
+                .nickname(signUpForm.getNickname())
+                .password(signUpForm.getPassword()) //TODO encoding 해야함
+                .studyCreatedByWeb(true)
+                .studyEnrollResultByWeb(true)
+                .studyUpdatedResultByWeb(true)
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+        return newAccount;
+    }
+
+    private void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("스터디올래, 회원가입 인증"); //메일 제목
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +
+                "&email="+ newAccount.getEmail());
+        javaMailSender.send(mailMessage);
+    }
+
+
+}
