d381e92164c8c94e62fee7a51db55134e69fecbf
goheeji <gmlwl0489@daum.net>
2021-03-17 22:56:43 +0900

    28. 패스워드 수정 완료

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -99,4 +99,9 @@ public class AccountService implements UserDetailsService {
     account.setProfileImage(profile.getProfileImage());
     accountRepository.save(account);
     }
+
+    public void updatePassword(Account account, String newPassword) {
+        account.setPassword(passwordEncoder.encode(newPassword));
+        accountRepository.save(account); //merge
+    }
 }
